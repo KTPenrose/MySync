ea3e6ea15c34a9f44d82bed178a60ade56e0a176
KTPenrose <kurt@kurtpenrose.com>
2021-04-23 22:23:09 -0400

    Remove extraneous comment

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
 mod my_sync_arg_parser;
 
 fn main() {
-    let args = std::env::args().skip(1);
-    //let mut user_instructions = my_sync_library::UserInstructions::new();
-    
-    let user_instructions=my_sync_arg_parser::parse_args(args);
-
+    let user_instructions=my_sync_arg_parser::parse_args(std::env::args().skip(1));
 
     println!("user_instructions=\n{}\n", user_instructions);   
 }
