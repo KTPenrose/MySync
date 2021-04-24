132f0ee2140faf26740941370711fb3dc4de30c6
KTPenrose <kurt@kurtpenrose.com>
2021-04-23 22:28:59 -0400

    Remove more comments and perform cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod my_sync_arg_parser;
 
 fn main() {
-    let user_instructions=my_sync_arg_parser::parse_args(std::env::args().skip(1));
+    let user_instructions = my_sync_arg_parser::parse_args(std::env::args().skip(1));
 
-    println!("user_instructions=\n{}\n", user_instructions);   
+    println!("user_instructions=\n{}\n", user_instructions);
 }
